3228e821bb663e3c43a831debd5c4dc5198eee5f
Gunter Grodotzki <gunter@grodotzki.co.za>
2017-10-22 21:55:49 +0200

    implement muc

--- files/sysvinit/60muc.sh
+++ files/sysvinit/60muc.sh
@@ -4,13 +4,19 @@ set -e
 
 test -f /etc/default/jabberd2 && . /etc/default/jabberd2
 
-NAME=c2s
-COMMAND=/usr/local/bin/jabberd2-${NAME}
-PIDFILE=/run/jabberd2/${NAME}.pid
+NAME=muc
+COMMAND=/usr/local/bin/mu-conference
+PIDPATH=/run/jabberd2
+PIDFILE=${PIDPATH}/${NAME}.pid
 CONFFILE=/etc/jabberd2/${NAME}.xml
 
+if [ ! -d ${PIDPATH} ]; then
+    mkdir -p ${PIDPATH}
+    chown ${USER}:${GROUP} ${PIDPATH}
+fi
+
 # exit now if we are not ment to run
-test "${C2S_RUN}" != 0 || exit 0
+test "${MUC_RUN}" != 0 || exit 0
 
 # check for executable
 test -f ${COMMAND} || exit 0
@@ -19,9 +25,9 @@ case "$1" in
    start)
 	printf " ${NAME}"
         if [ ! `pidof ${COMMAND}` ]; then
-      	    start-stop-daemon -b -c ${USER}:${GROUP} \
-        	--start --pidfile ${PIDFILE} --quiet \
-        	--exec ${COMMAND} -- -c ${CONFFILE} || printf "<Failed>"
+        	start-stop-daemon -b -c ${USER}:${GROUP} \
+        	    --start --pidfile ${PIDFILE} --quiet \
+        	    --exec ${COMMAND} -- -c ${CONFFILE} || printf "<Failed>"
         fi
 	;;
    stop)
@@ -30,22 +36,22 @@ case "$1" in
             --signal 15 --retry 3 --pidfile ${PIDFILE} \
             --exec ${COMMAND} || printf "<Failed>"
 	;;
-    status)
-        printf "${NAME}"
+   status)
+	printf "${NAME}"
         if [ `pidof ${COMMAND}` ]; then
-            printf " running\n"
+            printf " running"
         else
-            printf " not running\n"
+            printf " not running"
         fi
 	;;
-    restart)
+   restart)
         $0 stop
         sleep 1
         $0 start
 	;;
-    *)
-	printf "`basename $0` called with unknown option {$1}\n"
-	exit 1
+   *)
+        printf "`basename $0` called with unknown option {$1}\n"
+        exit 1
 	;;
 esac
 
